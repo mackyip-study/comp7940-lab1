94f72686f23e5ee342bab8962465c07db5837972
Mack YIP <study.mackyip@gmail.com>
2025-02-09 22:41:03 +0800

    Complete: Ex 1

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 def main():
     print("Hello World")
 
+#Lab1 - Python Exercise Ex 1
+# Find the all factors of x using a loop and the operator %
+# % means find remainder, for example 10 % 2 = 0; 10% 3 = 1
+def find_factor():
+    # print("========== Start ==========")
+
+    x = 52633
+    factors = []
+    for i in range(x+1):
+        if i == 0:
+            continue
+
+        if x % i == 0:
+            factors.append(i)
+
+    # print(factors)
+
+    # print("========== End ==========")
+
 if __name__ == '__main__':
-    main()
+    find_factor()
