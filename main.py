4d48791c9db174c4b55f3cafa14947a5a7013b80
Mack YIP <study.mackyip@gmail.com>
2025-02-09 22:45:33 +0800

    Complete: Ex2

--- main.py
+++ main.py
@@ -20,5 +20,22 @@ def find_factor():
 
     # print("========== End ==========")
 
+#Lab1 - Python Exercise Ex 2
+# Write a function that prints all factors of the given parameter x
+def print_factor(x):
+    print("========== Start ==========")
+
+    factors = []
+    for i in range(x+1):
+        if i == 0:
+            continue
+
+        if x % i == 0:
+            factors.append(i)
+
+    print(factors)
+
+    print("========== End ==========")
+
 if __name__ == '__main__':
-    find_factor()
+    print_factor(52633)
